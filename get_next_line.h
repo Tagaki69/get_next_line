8bc6a89eca8dd14bef5fbebaaeee065ef9854d82
Tagaki69 <enzo.larue74@gmail.com>
2025-12-03 15:17:31 +0100

    correction many bugs

--- get_next_line.h
+++ get_next_line.h
@@ -1,12 +1,40 @@
-#ifndef get_next_line
-# define BUFFER_SIZE 1024
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elarue <elarue@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/03 11:44:24 by elarue            #+#    #+#             */
+/*   Updated: 2025/12/03 11:59:10 by elarue           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-# include <unistd.h>
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# if BUFFER_SIZE > 1000000
+#  undef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000
+# endif
+
 # include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+char	*read_and_update_storage(int fd, char *storage);
+char	*extract_line_from_storage(char *storage);
+char	*update_storage_after_extraction(char *storage);
 
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *s);
 
 #endif
